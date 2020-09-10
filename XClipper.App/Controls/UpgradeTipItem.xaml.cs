8c441ee72da645fc42fa8dd0a99a598d38cdf8b0
Kaustubh Patange <developerkp16@gmail.com>
2020-09-10 21:12:01 +0530

    [Refactoring] Single license system

---
+++ XClipper.App/Controls/UpgradeTipItem.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Components.Controls
+{
+    /// <summary>
+    /// Interaction logic for UpgradeTipItem.xaml
+    /// </summary>
+    public partial class UpgradeTipItem : UserControl
+    {
+        public UpgradeTipItem()
+        {
+            InitializeComponent();
+        }
+    }
+}
