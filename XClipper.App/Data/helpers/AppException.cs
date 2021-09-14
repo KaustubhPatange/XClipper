3bcf5db7236c3e0c7f2a57508442f14bb5a34fde
Kaustubh Patange <developerkp16@gmail.com>
2021-09-14 23:54:19 +0530

    [Windows] Added new crash reporting server

--- XClipper.App/Data/helpers/AppException.cs
+++ XClipper.App/Data/helpers/AppException.cs
@@ -4,17 +4,17 @@ using System.Windows;
 using System.Windows.Threading;
 using static Components.Constants;
 using System;
-using System.Windows.Input;
 using System.Management;
 using System.Dynamic;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Reflection;
 using System.Threading.Tasks;
 using RestSharp;
-using System.Runtime.Remoting.Channels;
 using static Components.Core;
 using static Components.DefaultSettings;
+using SharpRaven;
+using SharpRaven.Data;
+using System.Linq;
 
 #nullable enable
 
@@ -40,6 +40,7 @@ namespace Components
 
         #endregion
 
+        private RavenClient ravenClient = new RavenClient(CRASH_SERVER);
         public void Attach()
         {
             AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
@@ -51,20 +52,22 @@ namespace Components
 
         private async void Winform_ThreadException(object sender, System.Threading.ThreadExceptionEventArgs e)
         {
-            await WriteCrashDetails("winform_report_", $"{e.Exception.Message}\n{e.Exception.StackTrace}").ConfigureAwait(false);
+            await WriteCrashDetails("winform_report_", $"{e.Exception.Message}\n{e.Exception.StackTrace}", e.Exception).ConfigureAwait(false);
         }
 
         private async void Current_DispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
         {
-            await WriteCrashDetails("app_report_", $"{e.Exception.Message}\n{e.Exception.StackTrace}").ConfigureAwait(false);
+            await WriteCrashDetails("app_report_", $"{e.Exception.Message}\n{e.Exception.StackTrace}", e.Exception).ConfigureAwait(false);
         }
 
         private async void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
         {
-            await WriteCrashDetails("domain_report_", e.ExceptionObject.ToString()).ConfigureAwait(false);
+            Exception? exception = null;
+            if (e.ExceptionObject is Exception) exception = e.ExceptionObject as Exception;
+            await WriteCrashDetails("domain_report_", e.ExceptionObject.ToString(), exception).ConfigureAwait(false);
         }
 
-        private async Task WriteCrashDetails(string prefix, string contents)
+        private async Task WriteCrashDetails(string prefix, string contents, Exception? exception = null)
         {
             new UWPToast.Builder(Application.Current.Dispatcher)
                .AddText(Translation.APP_CRASH)
@@ -86,24 +89,28 @@ namespace Components
                     if (obj.ScreenWidth != null)
                         dib.AppendLine(
                             $"Display: {obj.ScreenWidth}x{obj.ScreenHeight}, {obj.SystemName}, {obj.MonitorType}");
+                    exception?.Data?.Add("Display", $"{obj.ScreenWidth}x{obj.ScreenHeight}, {obj.SystemName}, {obj.MonitorType}");
                 }
 
                 // Memory
                 foreach (dynamic obj in GetAllObjects("Win32_PhysicalMemory"))
                 {
                     dib.AppendLine($"Memory: {((long) obj.Capacity).ToFileSizeApi()} {obj.Speed} MHz");
+                    exception?.Data?.Add("Memory", $"{((long)obj.Capacity).ToFileSizeApi()} {obj.Speed} MHz");
                 }
 
                 // Video Controller
                 foreach (dynamic obj in GetAllObjects("Win32_VideoController"))
                 {
                     dib.AppendLine($"VideoController: {obj.Name}, {((long) obj.AdapterRAM).ToFileSizeApi()}");
+                    exception?.Data?.Add("VideoController", $"{obj.Name}, {((long)obj.AdapterRAM).ToFileSizeApi()}");
                 }
 
                 // Processor
                 foreach (dynamic obj in GetAllObjects("Win32_Processor"))
                 {
                     dib.AppendLine($"ChipSet: {obj.Name} Threads/Cores: ({obj.ThreadCount}/{obj.NumberOfCores})");
+                    exception?.Data?.Add("ChipSet", $"{obj.Name} Threads/Cores: ({obj.ThreadCount}/{obj.NumberOfCores})");
                 }
 
                 // BIOS
@@ -129,6 +136,12 @@ namespace Components
             string crashFilePath = Path.Combine(ApplicationExceptionDirectory, prefix + CreateCrashSuffix() + ".txt");
 
             File.WriteAllText(crashFilePath, dib.ToString());
+
+            // Filter our exceptions
+
+            if (ignoreExceptionsStrings.Any(c => contents.Contains(c))) return;
+
+            await ravenClient.CaptureAsync(new SentryEvent(exception)).ConfigureAwait(false);
 
             await SendReport(Environment.UserName, dib.ToString()).ConfigureAwait(false);
         }
@@ -174,5 +187,7 @@ namespace Components
         }
 
         #endregion
+
+        private readonly string[] ignoreExceptionsStrings = new[] { "" }; // @"C:\Program Files\XClipper\locales", @"C:\Windows\system32\locales" TODO: Add this if verified
     }
 }
