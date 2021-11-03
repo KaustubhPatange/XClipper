43124a9716b3d211b16810c014ebb75ec93ed6bf
Kaustubh Patange <developerkp16@gmail.com>
2021-11-03 19:55:28 +0530

    configuring some gradle scripts

--- XClipper.Android/buildSrc/src/main/java/CoreVersion.kt
+++ XClipper.Android/buildSrc/src/main/java/CoreVersion.kt
@@ -2,6 +2,5 @@ object CoreVersion {
     const val KOTLIN = "1.5.20"
     const val ANDROID_COROUTINES = "1.4.1"
 
-    const val JETPACK_NAVIGATION = "2.3.4"
-    const val HILT = "2.36"
+    const val HILT = "2.39.1"
 }
