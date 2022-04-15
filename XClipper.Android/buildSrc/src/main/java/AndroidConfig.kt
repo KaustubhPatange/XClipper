52aa92fdf2901796a0f51198315bb579fa72e4aa
Kaustubh Patange <developerkp16@gmail.com>
2022-04-15 16:51:55 +0530

    [Bump] App Version

--- XClipper.Android/buildSrc/src/main/java/AndroidConfig.kt
+++ XClipper.Android/buildSrc/src/main/java/AndroidConfig.kt
@@ -4,8 +4,8 @@ object AndroidConfig {
     const val TARGET_SDK_VERSION = 30
     const val BUILD_TOOLS_VERSION = "30.0.3"
 
-    const val VERSION_CODE = 29
-    const val VERSION_NAME = "1.2.4"
+    const val VERSION_CODE = 30
+    const val VERSION_NAME = "1.2.5"
 
     const val ID = "com.kpstv.xclipper"
     const val TEST_INSTRUMENTATION_RUNNER = "androidx.test.runner.AndroidJUnitRunner"
